9afeac40f2ef0adb8fe43d5e2a6ec00d5ecef948
Julian Hahn <julian@seoulrobotics.org>
2023-05-03 17:41:34 +0900

    move conversion to package

--- go/main.go
+++ go/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"shp_to_geojson"
 )
 
 func main() {
-	fmt.Println(shp_to_geojson.ParseFromBytes())
+	bytes, err := os.ReadFile("./A1_NODE.shp")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(shp_to_geojson.ParseFromBytes(bytes))
 }
